fa76e408cd9801acb7d311a19c8948d72fe96c68
Caleb Waters <cw@mekanation.com>
2020-09-18 15:24:31 -0500

    Prioritied the cors filter before security filter

--- src/main/java/restclient/CorsFilter.java
+++ src/main/java/restclient/CorsFilter.java
@@ -1,11 +1,13 @@
 package restclient;
 
+import javax.annotation.Priority;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
+@Priority(500)
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
